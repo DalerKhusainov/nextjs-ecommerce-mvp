3b7ecdd7287745a3a2b3aadf936045b58d010b83
DalerKhusainov <dalerkhusainov85@gmail.com>
2024-04-20 20:01:53 +0200

    Installed all packages and dependencies. Created admin navigation

---
+++ prisma/migrations/20240420144509_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "priceInCents" INTEGER NOT NULL,
+    "filePath" TEXT NOT NULL,
+    "imagePath" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "isAvailableForPurchase" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "pricePaidInCents" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
